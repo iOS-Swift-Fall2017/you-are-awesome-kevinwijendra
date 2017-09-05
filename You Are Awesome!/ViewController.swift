42c317bbd4329072c2d963cafc3a0d1eeb48a54f
Kevin Wijendra <wijendra@bc.edu>
2017-09-05 15:21:46 -0400

    Week 2: Up to video 2.11 (Non-Repeating Random Variables)

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        } while lastNumber == newIndex
+        return newIndex
     }
-
-
+    
+    
+    @IBAction func messagebuttonPressed(_ sender: UIButton) {
+        let messages = ["You Are Awesome!",
+                        "You Are Fantastic!",
+                        "You Are Great",
+                        "You are the Best Programmer Ever",
+                        "You're Amazing",
+                        "This Program is Awesome",
+                        "Swift/iOS Programming is Cool"]
+        
+        // Showing new messages without repeating last message (Uses nonRepeatingRandom funciton we made above)
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
+        messageLabel.text = messages[index]
+    }
+    
 }
 
