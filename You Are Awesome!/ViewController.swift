fdba75412b3e1e9a8d15040a8f2c633fe5253a90
Kevin Wijendra <wijendra@bc.edu>
2017-09-12 16:08:42 -0400

    End of You Are Awesome App

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,16 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var awesomeImage: UIImageView!
+    @IBOutlet weak var soundSwitch: UISwitch!
+    var awesomePlayer = AVAudioPlayer()
     var index = -1
+    var imageNumber = -1
+    var soundNumber = -1
+    let numberOfImages = 5
+    let numberOfSounds = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // Play Sounds Function
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    
+    // Random Number Function
     func nonRepeatingRandom(lastNumber: Int, maxValue: Int) -> Int {
         var newIndex = -1
         repeat {
@@ -25,6 +48,13 @@ class ViewController: UIViewController {
         return newIndex
     }
     
+    
+    @IBAction func soundSwitchPressed(_ sender: UISwitch) {
+        if !soundSwitch.isOn && soundNumber != -1 {
+            // stop playing
+            awesomePlayer.stop()
+        }
+    }
     
     @IBAction func messagebuttonPressed(_ sender: UIButton) {
         let messages = ["You Are Awesome!",
@@ -35,9 +65,23 @@ class ViewController: UIViewController {
                         "This Program is Awesome",
                         "Swift/iOS Programming is Cool"]
         
-        // Showing new messages without repeating last message (Uses nonRepeatingRandom funciton we made above)
+        // Showing Messages Using Function
         index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
         messageLabel.text = messages[index]
+        
+        // Showing Images Using Function
+        awesomeImage.isHidden = false
+        imageNumber = nonRepeatingRandom(lastNumber: imageNumber, maxValue: numberOfImages)
+        awesomeImage.image = UIImage(named: "image\(imageNumber)")
+        
+        
+        // Playing Sound Using Function
+        if soundSwitch.isOn {
+            soundNumber = nonRepeatingRandom(lastNumber: soundNumber, maxValue: numberOfSounds)
+            let soundName = "sound\(soundNumber)"
+            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
+        }
+        
     }
     
 }
